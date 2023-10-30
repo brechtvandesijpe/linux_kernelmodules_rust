3916a3a9f260fbbaacc88424aa5f342044ed4f02
brechtvandesijpe <brecht.vandesijpe@student.kuleuven.be>
2023-10-30 17:32:15 +0000

    Step 4: minimal fops implementation

--- rust_scull.rs
+++ rust_scull.rs
@@ -1,4 +1,5 @@
 use kernel::prelude::*;
+use kernel::file;
 
 module! {
     type: Scull,
@@ -8,9 +9,17 @@ module! {
 
 struct Scull;
 
+[#vtable]
+impl file::Operations for Scull {
+    fn open(_context: &(), _file: &file::file) -> Result {
+        pr_info("File was opened!\n");
+        Ok(())
+    }
+}
+
 impl kernel::Module for Scull {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
-        pr_info!("Hello scull!");
+        pr_info!("Hello scull!\n");
         Ok(Self)
     }
 }
