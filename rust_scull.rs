6028583103fa0704451b0f9fb34a71762bb3b481
brechtvandesijpe <brecht.vandesijpe@student.kuleuven.be>
2023-10-30 17:25:48 +0000

    Step 3: print hello scull when booting

--- rust_scull.rs
+++ rust_scull.rs
@@ -10,6 +10,7 @@ struct Scull;
 
 impl kernel::Module for Scull {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
+        pr_info!("Hello scull!");
         Ok(Self)
     }
 }
