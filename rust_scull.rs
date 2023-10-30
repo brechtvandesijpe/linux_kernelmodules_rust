109a84eff3a4e88c0e350aa8d6165c8027597ae1
brechtvandesijpe <brecht.vandesijpe@student.kuleuven.be>
2023-10-30 17:09:17 +0000

    Step 2: module declaration and init

---
+++ rust_scull.rs
@@ -0,0 +1,15 @@
+use kernel::prelude::*;
+
+module! {
+    type: Scull,
+    name: "scull",
+    license: "GPL",
+}
+
+struct Scull;
+
+impl kernel::Module for Scull {
+    fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
+        Ok(Self)
+    }
+}
